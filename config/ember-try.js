a6d12692399d80d0c3050ac80ce8cd1de933dbaa
Phillip Haines <philliphaines@users.noreply.github.com>
2020-10-21 13:02:36 +0100

    update deps and lts test versions

--- config/ember-try.js
+++ config/ember-try.js
@@ -4,20 +4,20 @@ const getChannelURL = require('ember-source-channel-url');
 
 module.exports = async function () {
   return {
-    scenarios: [
-      {
-        name: 'ember-lts-3.12',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.12.0'
-          }
-        }
-      },
+    scenarios: [      
       {
         name: 'ember-lts-3.16',
         npm: {
           devDependencies: {
             'ember-source': '~3.16.0'
+          }
+        }
+      },
+      {
+        name: 'ember-lts-3.20',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.20.0'
           }
         }
       },
