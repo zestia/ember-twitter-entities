c9fe01dc43bc8d5b3492f8582494b81ca0f3fdca
Andrew Kirwin <github@andrewkirwin.co.uk>
2023-04-21 09:26:26 +0100

    Use hash locationType in prod

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -42,6 +42,7 @@ module.exports = function (environment) {
 
   if (environment === 'production') {
     ENV.rootURL = '/ember-twitter-entities/';
+    ENV.locationType = 'hash';
   }
 
   return ENV;
